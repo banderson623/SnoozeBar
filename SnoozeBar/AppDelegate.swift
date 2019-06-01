0f88fcd842a60bf708077548a4601671c533604a
Brian Anderson <brian.anderson@voxmedia.com>
2019-06-01 12:58:00 -0500

    Initial Commit

---
+++ SnoozeBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SnoozeBar
+//
+//  Created by Brian Anderson on 6/1/19.
+//  Copyright © 2019 Brian Anderson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
